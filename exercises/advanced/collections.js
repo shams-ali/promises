d24de0c10af351bbf261b61661788879da7fbcb8
shams-ali <shams.ali0216@gmail.com>
2016-05-12 20:05:26 -0700

    completes advanced colleciton.js promises.all()

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,12 +7,29 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
+var fs = require('fs');
+var Promise = require('bluebird');
+var promFS = Promise.promisifyAll(require('fs'));
 
+var nodeStyle = require('../bare_minimum/callbackReview.js');
+var pluckFirstLineFromFileAsync = Promise.promisify(nodeStyle.pluckFirstLineFromFile);
+var getStatusCodeAsync = Promise.promisify(nodeStyle.getStatusCode);
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+  var promises = [];
+  //console.log(filePaths, '<--- filePaths');
+  filePaths.forEach( function(filepath) {
+    promises.push(pluckFirstLineFromFileAsync(filepath));
+  });
+  console.log('this is promise array --->', promises);
+  return Promise.all( promises )
+    .then( (result) => {
+      console.log('result outside of Promise.all ---->', result);
+      result = result.join('\n');
+      return promFS.writeFileAsync(writePath, result);
+    })
+    .catch(err => console.log(err));
 };
-
 // Export these functions so we can unit test them
 module.exports = {
   combineFirstLineOfManyFiles: combineFirstLineOfManyFiles
